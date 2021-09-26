fa91355d8587decba984ec6030e53a05bb84ea8c
ayush agrawal <ayushagrawal2525@gmail.com>
2021-09-26 22:18:48 +0530

    first commit

---
+++ src/main/java/com/MockProject/demo/SortImpl.java
@@ -0,0 +1,23 @@
+package com.MockProject.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.MockProject.demo.services.Checkersi;
+import com.MockProject.demo.services.SortAlgorithm;
+
+@Component
+public class SortImpl {
+SortAlgorithm sortAlgo;
+@Autowired
+public SortImpl(SortAlgorithm sortAlgorithm)
+{
+	this.sortAlgo=sortAlgorithm;
+}
+public void printMessage()
+{
+	System.out.println("h");
+System.out.println(sortAlgo.Sort());
+}
+}
